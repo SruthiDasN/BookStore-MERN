ea8050fb9bdb1ef16a67cdd0283887acd0f4ff2b
Sruthi Das N <sruthidas2705@gmail.com>
2023-10-04 12:43:38 +0530

    Frontend create, edit, show book is completed

--- backend/index.js
+++ backend/index.js
@@ -18,16 +18,16 @@ app.use(express.json());
 
 //middleware for handling CORS POLICY
 //Option 1: Allow all Origins with default of cors(*)
-// app.use(cors());
+app.use(cors());
 
 //Option 2: Allow custom origins
-app.use(
-    cors({
-        origin: 'http://localhost:3000',
-        methods: ['GET', 'POST', 'PUT', 'DELETE'],
-        allowedHeaders: ['Content-Type'],
-    })
-);
+// app.use(
+//     cors({
+//         origin: 'http://localhost:3000',
+//         methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//         allowedHeaders: ['Content-Type'],
+//     })
+// );
 
 
 
